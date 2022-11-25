4aa63ca51e862e6b7b102a31e57bf291c254d097
Elena Donyakina <hellengoll@gmail.com>
2022-11-25 04:21:02 +0300

    changed test_longest_length

--- tests/test_longest_length.py
+++ tests/test_longest_length.py
@@ -1,8 +1,9 @@
 from brain_tasks.tasks.longest_length import find_longest_length
 
 
-assert find_longest_length('jabcjdelg') == 8
+def test_find_longest_length():
+    assert find_longest_length('jabcjdelg') == 8
 
-assert find_longest_length('') == 0
 
-print('Все тесты пройдены!')
+def test_find_longest_length_for_empty_stryng():
+    assert find_longest_length('') == 0
